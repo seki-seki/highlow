8f15c41e3e912b0ed00ec23eaabea2a8949b33dc
seki <schokolade0317@gmail.com>
2021-02-12 15:33:18 +0900

    made hit col

--- src/component/home/cards/previousGameResults.jsx
+++ src/component/home/cards/previousGameResults.jsx
@@ -5,8 +5,7 @@ import {makeDotByDecimal} from "../../../common/helper/decimalHelper";
 import Side from "../../../common/constants/Side";
 
 const PreviousGameResults = () => {
-  const {previousGames,game} = HighlowContainer.useContainer();
-  console.log(previousGames)
+  const {previousGames, game, myBets} = HighlowContainer.useContainer();
   return <Card>
     <h2>Previous Game Results</h2>
     <Table>
@@ -15,15 +14,25 @@ const PreviousGameResults = () => {
         <TableCell>TIME</TableCell>
         <TableCell>PRICE({game.pairName})</TableCell>
         <TableCell>RESULT</TableCell>
+        <TableCell>HIT</TableCell>
       </TableHeader>
-      {previousGames?.map((game,i) => (
-        <TableColumn key={i}>
-          <TableCell>{game.gameIndex}</TableCell>
-          <TableCell>{dateFormat(new Date(Number.parseInt(game.resultTimestamp)),"mm/dd HH:MM:ss")}</TableCell>
-          <TableCell>{makeDotByDecimal(game.currentPrice, game.decimal)} -> {!game.finished ? "Loading...": makeDotByDecimal(game.resultPrice, game.decimal)}</TableCell>
-          <TableCell>{!game.finished ? "Loading...": game.winner === Side.high ? "UP" : game.winner === Side.low ? "DOWN" : game.winner === Side.draw ? "DRAW" : "NO GAME"}</TableCell>
-        </TableColumn>
-      ))}
+      {previousGames?.map((game, i) => {
+        const betsOnThisGame = myBets?.filter(bet => Number.parseInt(bet.gameIndex) === game.gameIndex);
+        const hasBetsOnThisGame = betsOnThisGame?.length > 0;
+        const betOnWinnerSide = betsOnThisGame?.every(bet => bet.side === game.winner);
+        const betOnLooserSide = betsOnThisGame?.every(bet => bet.side !== game.winner);
+        const isNoGame = game.winner === Side.noGame;
+
+        return (
+          <TableColumn key={i}>
+            <TableCell>{game.gameIndex}</TableCell>
+            <TableCell>{dateFormat(new Date(Number.parseInt(game.resultTimestamp)), "mm/dd HH:MM:ss")}</TableCell>
+            <TableCell>{makeDotByDecimal(game.currentPrice, game.decimal)} -> {!game.finished ? "Loading..." : makeDotByDecimal(game.resultPrice, game.decimal)}</TableCell>
+            <TableCell>{!game.finished ? "Loading..." : game.winner === Side.high ? "UP" : game.winner === Side.low ? "DOWN" : game.winner === Side.draw ? "DRAW" : "NO GAME"}</TableCell>
+            <TableCell>{!game.finished ? "..." : !hasBetsOnThisGame || isNoGame ? "-" : betOnWinnerSide ? "○" : betOnLooserSide ? "☓" : "△"}</TableCell>
+          </TableColumn>
+        )
+      })}
     </Table>
   </Card>
 };
@@ -39,7 +48,7 @@ const Table = styled.div`
 `;
 
 const TableCell = styled.div`
-  width: 25%;
+  width: 20%;
 `;
 const TableHeader = styled.div`
   display: flex;
